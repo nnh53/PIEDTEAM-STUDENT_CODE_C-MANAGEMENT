6aaaea511e0799a82fd87e66b50619f913755ca1
EmTanIT <tan182205@gmail.com>
2024-01-03 16:00:16 +0700

    Class Summary Feature

--- src/testmanagements/testManagement.controllers.ts
+++ src/testmanagements/testManagement.controllers.ts
@@ -7,6 +7,45 @@ import { ErrorWithStatus } from '~/error/error.model'
 
 const testManagementsServicesInstance = TestManagementsServices.getInstance()
 
+export const getClassResultController = async (req: Request, res: Response) => {
+  const testPrepare = {
+    main1: {
+      testCases: ['1234\n'],
+      expectedValues: ['Nhap so n: \r\n10']
+    },
+    main2: {
+      testCases: ['6\n'],
+      expectedValues: ['Nhap so n: \r\n8']
+    },
+    main3: {
+      testCases: ['9\n', '5\n'],
+      expectedValues: ['Nhap so n: \r\nKhong phai so nguyen to', 'Nhap so n: \r\nLa so nguyen to']
+    },
+    main4: {
+      testCases: ['10\n'],
+      expectedValues: ['Nhap so n: \r\n1\r\n3\r\n5\r\n7\r\n9']
+    }
+  }
+  const classResult = await getClassResult(UPLOAD_FILE_DIR, testPrepare)
+  if (classResult === null) {
+    return res.json({
+      message: 'Student has not submitted yet'
+    })
+  }
+  const classSummary = {} as { [key: string]: number }
+  for (const key in classResult) {
+    if (classResult[key] !== undefined) {
+      const questionResult = classResult[key]
+      const questionSummary = questionResult.filter((item) => item.fail.length == 0)
+      classSummary[key] = questionSummary.length
+    }
+  }
+  return res.json({
+    message: 'Class Result !',
+    classSummary
+  })
+}
+
 export const uploadFileController = async (req: Request, res: Response) => {
   // const file = await testManagementsServicesInstance.uploadFileService(req)
   // const executedPath = getExePath(file.path)
--- src/testmanagements/testManagement.routes.ts
+++ src/testmanagements/testManagement.routes.ts
@@ -1,17 +1,19 @@
 import { Router } from 'express'
 import { wrapAsync } from '~/utils/handlers'
-import { uploadFileController } from './testManagement.controllers'
+import { getClassResultController, uploadFileController } from './testManagement.controllers'
 
 const testManagementRouter = Router()
 
 //Get all Questions of the test
 testManagementRouter.get('/:testId', (req, res) => {})
 
-testManagementRouter.get('/', (req, res) => {
-  console.log('test')
-})
+// testManagementRouter.get('/', (req, res) => {
+//   console.log('test')
+// })
 
 //Adding File to the question
 testManagementRouter.post('/addFile', wrapAsync(uploadFileController))
+
+testManagementRouter.get('/', wrapAsync(getClassResultController))
 
 export default testManagementRouter
